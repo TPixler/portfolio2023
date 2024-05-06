ac2a782fd0d105d60099990c01a5b9dac798debb
TPixler <123421607+TPixler@users.noreply.github.com>
2024-05-06 11:39:09 -0500

    added some pic to the just for fun section

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -6,6 +6,8 @@ import github from "../images/github.png";
 import "./Contact.css";
 import yay2 from "../images/yay2.png";
 import quote from "../images/quote.jpg";
+import funnycode from "../images/funnycodepic.jpg";
+import funnypic from "../images/funnypic.jpg";
 
 export default function Contact() {
   return (
@@ -13,6 +15,10 @@ export default function Contact() {
       <p className="justForFun">Just for Fun</p>
       <img className="quotepic" src={quote} alt="quote"></img>
       <img className="funny" src={yay2} alt="funnypic"></img>
+      <img className="funnycodepic" src={funnycode} alt="funnypic1"></img>
+      <img className="funnypic" src={funnypic} alt="funnypic"></img>
+
+
       <ContactForm />
     </div>
   );
